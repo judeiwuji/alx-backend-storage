2fb535beda05893f4ef3008472c8605cebedb079
judeiwuji <judeiwuji@gmail.com>
2023-04-06 08:40:15 -0700

    error fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,8 @@
 -- a SQL script that lists all bands with Glam rock
 -- as their main style, ranked by their longevity
-
 SELECT `band_name`, 
 IFNULL(`split`, 2020) - IFNULL(`formed`, 0) AS `lifespan` 
 FROM `metal_bands` 
-WHERE LOWER(`style`) LIKE '%glam rock%'
+WHERE LOWER(`style`) LIKE '%Glam rock%'
 ORDER BY 
 IFNULL(`split`, 2020) - IFNULL(`formed`, 0) DESC;
