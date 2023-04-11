d6f3256b838e2615a6baf358ad8bf72761d7e33a
judeiwuji <judeiwuji@gmail.com>
2023-04-11 16:16:05 -0700

    Add: Task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Where can I learn Python"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """function that returns the list of school 
+    having a specific topic
+
+    Args:
+        mongo_collection(Collection)
+        topic(str)
+    """
+    return mongo_collection.find({"topics": topic})
