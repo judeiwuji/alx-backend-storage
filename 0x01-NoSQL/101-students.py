f9fa5da78be6e60c901b6d5d3e8192a9e5e63c35
judeiwuji <judeiwuji@gmail.com>
2023-04-12 12:14:16 -0700

    Add: Task 101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Top students"""
+
+
+def top_students(mongo_collection):
+    """function that returns all students sorted
+    by average score
+
+    Args:
+        mongo_collection(Collection)
+    """
+    return mongo_collection.aggregate(
+        [{"$project": {"_id": "$_id", "name": 1, "averageScore": {"$avg": "$topics.score"}}}])
